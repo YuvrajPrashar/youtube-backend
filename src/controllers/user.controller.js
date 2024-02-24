f7254c6cbc96dcfa5e90a95bc1df2e571da89a22
YuvrajPrashar <yuvrajprashar97@gmail.com>
2024-02-24 22:38:11 +0530

    commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,18 @@
 import asyncHandler from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "hehehe",
-  });
+  // get data from frontend
+  // validation - not empty
+  // check if user already exists: check username and email
+  // check for images , check for avatar
+  // upload to cloudinary,avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return res
+
+  const { fullName, email, username, password } = req.body;
+  console.log("email:", email);
 });
 
 export { registerUser };
